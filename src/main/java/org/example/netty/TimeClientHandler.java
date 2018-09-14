5e15831fa90e86c8b3694b0697d0ac99fea51ce9
zengpeng <zengpengapple@qq.com>
2018-09-14 14:39:01 +0800

    aa

--- src/main/java/org/example/netty/TimeClientHandler.java
+++ src/main/java/org/example/netty/TimeClientHandler.java
@@ -34,14 +34,7 @@ public class TimeClientHandler extends ChannelHandlerAdapter{
 	        cause.printStackTrace();
 	        ctx.close();
 	    }
-	    
-	    
-	    public void test001() {
-	    	
-	    }
 	
-	public void test001() {
-		
-	}
+	
 }
 
