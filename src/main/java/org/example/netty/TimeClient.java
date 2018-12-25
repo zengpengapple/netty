406e68d1ef6465ca2b75b57e38127bfa782aea84
zengpengapple@qq.com <zp6636371>
2018-12-25 22:37:00 +0800

    aaa

--- src/main/java/org/example/netty/TimeClient.java
+++ src/main/java/org/example/netty/TimeClient.java
@@ -23,6 +23,7 @@ public class TimeClient {
 	 public static void main(String[] args) throws Exception {
 		    String str ="127.0.0.1";
             String abc ="127.0.0.1";
+            String a = "hello";
 	        String host ="127.0.0.1";
 	        int port = 8080;
 	        EventLoopGroup workerGroup = new NioEventLoopGroup();
